87f4a3f3c241804b4366f136565260ae81f92cb7
Li Heng <liheng6567@gmail.com>
2020-03-17 21:32:49 +0800

    Prove that SLF4J logger and LOG4J logger can access same log file

--- src/main/java/me/liheng/TryLog.java
+++ src/main/java/me/liheng/TryLog.java
@@ -1,17 +1,29 @@
 package me.liheng;
 
+import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TryLog {
 
     private static final Logger logger = LoggerFactory.getLogger(TryLog.class);
+    private static final org.apache.logging.log4j.Logger log4jLogger = LogManager.getLogger(TryLog.class);
 
     public static void main(String[] args) throws InterruptedException {
+        testLog4j();
+    }
 
-        for(int i = 0; i < 20; i++) {
+    private static void testRollingFileAppender() throws InterruptedException {
+        for(int i = 0; i < 2000; i++) {
             logger.info("This is the " + i + " time I say 'Hello World'.");
-//            Thread.sleep(70);
+            Thread.sleep(70);
         }
+    }
+
+    private static void testLog4j() {
+        logger.info("This is from SLF4J logger");
+        log4jLogger.info("This is from LOG4J logger");
+        logger.info("This is from SLF4J logger");
+        log4jLogger.info("This is from LOG4J logger");
     }
 }
